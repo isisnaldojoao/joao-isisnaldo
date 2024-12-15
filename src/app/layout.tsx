a486b5b247f62866f3ef434f6f90b5889386059c
isisnaldojoao <isisnaldojoao@gmail.com>
2024-12-15 15:36:33 -0300

    third version add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,20 @@
 import { Header } from "@/components/header";
 import "./globals.css";
 
+const metadata = {
+  title: "João Isisnaldo",
+  description: "FullStack developer!",
+  keywords: "Next.js, SEO, React, Programação",
+  author: "João Isisnaldo",
+  openGraph: {
+    title: "João Isisnaldo",
+    description: "FullStack developer!",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
